11e702b5ee05f8dfc0d1799c3d6e956102ba5bf7
camillemoutahir <moutahic@3il.fr>
2025-05-06 14:38:17 +0200

    commit v1 appli

--- src/app/add-flower/add-flower.component.ts
+++ src/app/add-flower/add-flower.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { FlowerService } from '../services/flowers.service';
+import { Flower } from '../models/flowers.model';
+import { FlowersComponent } from '../flowers/flowers.component';
 
 @Component({
   selector: 'app-add-flower',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './add-flower.component.html',
   styleUrl: './add-flower.component.scss'
 })
 export class AddFlowerComponent {
 
+  @Output() flowerAdded = new EventEmitter<void>();
+
+  newFlower: Flower = {
+    name: '',
+    image_url: '',
+    description: ''
+  };
+
+  constructor(private flowerService: FlowerService,
+     private flowerComponent: FlowersComponent) {}
+
+  addFlower() {
+    this.flowerService.addFlower(this.newFlower).subscribe(() => {
+      this.newFlower = { name: '', image_url: '', description: '' };
+      this.flowerAdded.emit();
+      this.flowerComponent.onFlowerAdded();
+    });
+  }
 }
--- src/app/flower-item/flower-item.component.ts
+++ src/app/flower-item/flower-item.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FlowerService } from '../services/flowers.service';
+import { Flower } from '../models/flowers.model';
 
 @Component({
   selector: 'app-flower-item',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './flower-item.component.html',
   styleUrl: './flower-item.component.scss'
 })
 export class FlowerItemComponent {
 
+  @Input() flower!: Flower;
+  @Output() flowerDeleted = new EventEmitter<void>();
+
+  constructor(private flowerService: FlowerService) {}
+
+  deleteFlower() {
+    this.flowerService.deleteFlower(this.flower.id!).subscribe(() => {
+      this.flowerDeleted.emit();
+    });
+  }
 }
